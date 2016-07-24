4616f8771de35d00cf5a7f7c8eab57734b329b5a
Volcomix <volcomix@gmail.com>
2016-07-24 19:44:06 +0200

    Fix todo item style

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -14,22 +14,31 @@ interface Props {
 const TodoItem: React.StatelessComponent<Props> = ({ todo }) => (
     <ListItem
         leftCheckbox={
-            <Checkbox checked={todo.isDone} />
+            <Checkbox
+                checked={todo.isDone}
+                style={{ top: 24 }}
+            />
         }
+        style={{ display: 'flex' }}
     >
         <TimePicker
             hintText='Heure de début'
             format='24hr'
             value={todo.startHour}
+            style={{ display: 'inline' }}
+            textFieldStyle={{ width: 120 }}
         />
         <TimePicker
             hintText='Durée'
             format='24hr'
             value={moment({ 'minutes': todo.duration }).toDate()}
+            style={{ display: 'inline' }}
+            textFieldStyle={{ width: 120 }}
         />
         <TextField
             hintText='Tâche'
             value={todo.task}
+            style={{ flexGrow: 1 }}
         />
         <TextField
             hintText='Détail'
