543c1a3e9c8c7e435748766fad1f1c6477aaabb3
Volcomix <volcomix@gmail.com>
2016-08-11 15:13:53 +0200

    Fix drag and drop start from handle

--- src/components/DraggableItem.tsx
+++ src/components/DraggableItem.tsx
@@ -19,76 +19,80 @@ export interface Callbacks {
     onDragEnd: React.DragEventHandler
 }
 
-const DraggableItem = ({
-    order,
-    height,
-    isDragging,
-    draggingY,
-    hover,
-    children,
-    onMouseOver,
-    onDragStart,
-    onDrag,
-    onDragEnd
-}: Props & Callbacks) => {
+class DraggableItem extends React.Component<Props & Callbacks, {}> {
 
-    let handle: HTMLDivElement
-    let dragTarget: Node
+    private handle: HTMLDivElement
+    private dragTarget: Node
 
-    return (
-        <Paper
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                position: 'absolute',
-                top: isDragging ? draggingY - height / 2 - 10 : order * height,
-                left: 0,
-                right: 0,
-                transition: isDragging ?
-                    'all 150ms ease-out, top 1ms linear' :
-                    'all 250ms ease-out',
-                padding: '10px 0px',
-                zIndex: isDragging ? 10000 : order,
-                backgroundColor: 'white'
-            }}
-            zDepth={isDragging ? 2 : 1}
-            draggable={true}
-            onMouseOver={onMouseOver}
-            onMouseDown={ev => dragTarget = ev.target as Node}
-            onDragStart={ev => {
-                if (handle.contains(dragTarget)) {
-                    ev.dataTransfer.effectAllowed = 'move'
+    render() {
+        const {
+            order,
+            height,
+            isDragging,
+            draggingY,
+            hover,
+            children,
+            onMouseOver,
+            onDragStart,
+            onDrag,
+            onDragEnd
+        } = this.props
 
-                    // Typescript definition does not declare setDragImage
-                    ;(ev.dataTransfer as any).setDragImage(new Image(), 0, 0)
-                    
-                    // Make it work on Firefox
-                    ev.dataTransfer.setData('text/plain', null)
+        return (
+            <Paper
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    position: 'absolute',
+                    top: isDragging ? draggingY - height / 2 - 10 : order * height,
+                    left: 0,
+                    right: 0,
+                    transition: isDragging ?
+                        'all 250ms ease-out, top 1ms linear' :
+                        'all 250ms ease-out',
+                    padding: '10px 0px',
+                    zIndex: isDragging ? 10000 : order,
+                    backgroundColor: 'white'
+                }}
+                zDepth={isDragging ? 2 : 1}
+                draggable={true}
+                onMouseOver={onMouseOver}
+                onMouseDown={ev => this.dragTarget = ev.target as Node}
+                onDragStart={ev => {
+                    if (this.handle.contains(this.dragTarget)) {
+                        ev.dataTransfer.effectAllowed = 'move'
 
-                    onDragStart(ev.pageY)
-                } else {
-                    ev.preventDefault()
-                }
-            }}
-            onDrag={ev => {
-                onDrag(ev.pageY, Math.floor((ev.pageY - 10) / height))
-            }}
-            onDragEnd={onDragEnd}
-        >
-            <div ref={node => handle = node}>
-                <ActionReorder
-                    style={{
-                        transition: 'initial',
-                        visibility: hover ? 'visible' : 'hidden',
-                        margin: 12,
-                        cursor: 'move'
-                    }}
-                    color={grey500}
-                />
-            </div>
-            {children}
-        </Paper>
-    )
+                        // Typescript definition does not declare setDragImage
+                        ;(ev.dataTransfer as any).setDragImage(new Image(), 0, 0)
+                        
+                        // Make it work on Firefox
+                        ev.dataTransfer.setData('text/plain', null)
+
+                        onDragStart(ev.pageY)
+                    } else {
+                        ev.preventDefault()
+                    }
+                }}
+                onDrag={ev => {
+                    onDrag(ev.pageY, Math.floor((ev.pageY - 10) / height))
+                }}
+                onDragEnd={onDragEnd}
+            >
+                <div ref={node => this.handle = node}>
+                    <ActionReorder
+                        style={{
+                            transition: 'initial',
+                            visibility: hover ? 'visible' : 'hidden',
+                            margin: 12,
+                            cursor: 'move'
+                        }}
+                        color={grey500}
+                    />
+                </div>
+                {children}
+            </Paper>
+        )
+    }
 }
 
 export default DraggableItem
